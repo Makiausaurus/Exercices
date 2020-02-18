534dcd009b168cb9371e71bdb7c490805653af7d
Gaulthier Guérin <gaulthier.guerin@gmail.com>
2020-02-18 15:03:48 +0100

    Ex_MagicNb
    ajout exercice RegExp

--- Exercices JS/Ex_EventMagicNb.js
+++ Exercices JS/Ex_EventMagicNb.js
@@ -1,13 +1,18 @@
-var magic = parseInt(Math.random() * 100)
+var magic = parseInt(Math.random() * 100);
 console.log(magic);
 
 // function verif() {
 document.getElementById("button1").addEventListener("click", () => {
     var input = document.getElementById("textBox1").value;
-
+    var filtre = new RegExp("^[1-9][0-9]?$|^100$");
+    var resultat = filtre.test(input);
+    console.log(resultat);
     do {
-        if (input == "") {
-            alert("Veuillez remplir le champ.")
+        if (resultat == false) {
+            alert("Veuillez entrer seulement un chiffre entre 1 et 100.");
+            break;
+        } else if (input == "") {
+            alert("Veuillez remplir le champ.");
             break;
         } else if (input == magic) {
             alert("Bravo !");
